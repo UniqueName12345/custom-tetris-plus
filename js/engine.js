38be0e41f9da99c551cf94fdcff72af3426f783c
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-03-08 14:10:00 +0800

    Change branding and implement soft drop

--- js/engine.js
+++ js/engine.js
@@ -53,20 +53,28 @@ Game.Engine.prototype.getNextType = function() {
 	return this._nextType;
 }
 
-Game.Engine.prototype.drop = function() {
+Game.Engine.prototype.drop = function(softDrop) {
 	if (!this._piece || this._dropping) { return; }
 
 	var gravity = new XY(0, -1);
-	while (this._piece.fits(this.pit)) {
+
+	if (softDrop) {
+		// If softDrop is true, move the piece down by one cell
 		this._piece.xy = this._piece.xy.plus(gravity);
+	} else {
+		// If SHIFT key is pressed along with DOWN arrow key, drop the piece to the bottom instantly
+		while (this._piece.fits(this.pit)) {
+			this._piece.xy = this._piece.xy.plus(gravity);
+		}
+		this._piece.xy = this._piece.xy.minus(gravity);
 	}
-	this._piece.xy = this._piece.xy.minus(gravity);
 
 	this._stop();
 	this._dropping = true;
 	setTimeout(this._drop.bind(this), Game.INTERVAL_DROP);
 	return this;
 }
+
 
 Game.Engine.prototype.rotate = function() {
 	if (!this._piece || this._dropping) { return; }
